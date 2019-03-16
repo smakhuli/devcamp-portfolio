6a0ef131d98e9f9028e753a8df40adf21dd43d85
Sami Makhuli <sami.makhuli@ibethel.org>
2019-03-16 15:52:52 -0700

    implemented friendly routes for blogs

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
 end
