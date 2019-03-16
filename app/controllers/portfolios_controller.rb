1a78ee9dbe193d141949b8997e1abff9b32589b9
Sami Makhuli <sami.makhuli@ibethel.org>
2019-03-16 13:19:34 -0700

    intergrate edit and update portfolio actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,9 +13,24 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to @portfolios_path, notice: 'Portfolio Item was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
       else
         format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
