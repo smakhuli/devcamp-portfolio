3f95bd4f9df832ad2a621798a4a29f02238608d9
Sami Makhuli <sami.makhuli@ibethel.org>
2019-03-16 16:52:37 -0700

    implemented custom toggle status route for blog

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   # get 'pages/contact'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   root to: 'pages#home'
 end
